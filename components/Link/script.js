ecba3372b804f0453d04a1794a60e775a7010f36
Justin Chiou <chiou.kai@gmail.com>
2019-06-12 16:19:05 -0400

    create a component for button links

---
+++ components/Link/script.js
@@ -0,0 +1,16 @@
+export default {
+  props: {
+    linkClass: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
