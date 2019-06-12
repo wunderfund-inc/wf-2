18c95aebe5aad3903cb6e13433f09a9aef45f92d
Justin Chiou <chiou.kai@gmail.com>
2019-06-12 16:19:44 -0400

    use local data to scaffold links

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,7 +1,15 @@
 import Logo from '~/components/Logo'
+import Link from '~/components/Link'
+import links from './links.json'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Link
+  },
+  data() {
+    return {
+      links: links
+    }
   }
 }
