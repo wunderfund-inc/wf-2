941e6598fcad7758ca1e57dc21c7aeabce81032f
Justin Chiou <chiou.kai@gmail.com>
2019-06-12 17:01:18 -0400

    remove pug and scss loader

---
+++ components/ButtonLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :class="linkClasses" :href="url">
+    {{ title }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    linkClasses: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- components/Logo/index.vue
+++
@@ -1,2 +0,0 @@
-<template src="./template.pug" lang="pug" />
-<style src="./style.scss" lang="scss" />
