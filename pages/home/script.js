73a4c9880eb480f4525cf65c09ae50481b691964
Justin Chiou <chiou.kai@gmail.com>
2019-06-12 16:32:52 -0400

    remove link component

--- pages/home/script.js
+++ pages/home/script.js
@@ -1,11 +1,9 @@
 import Logo from '~/components/Logo'
-import Link from '~/components/Link'
 import links from './links.json'
 
 export default {
   components: {
-    Logo,
-    Link
+    Logo
   },
   data() {
     return {
