7500e5d327192283c320dcd21cfc475ed0fda019
Justin Chiou <chiou.kai@gmail.com>
2019-06-13 10:48:22 -0400

    test the button link too

--- components/ButtonLink/test.spec.js
+++ components/ButtonLink/test.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import Component from './index'
 
-describe('Logo Component', () => {
+describe('ButtonLink Component', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Component)
     expect(wrapper.isVueInstance()).toBeTruthy()
