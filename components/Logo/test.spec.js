291728c6b4426f6b253d11d5e818795e859eeadb
Justin Chiou <chiou.kai@gmail.com>
2019-06-13 10:25:39 -0400

    test logo component

--- components/Logo/test.spec.js
+++ components/Logo/test.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Logo from '@/components/Logo'
 
-describe('Logo', () => {
+describe('Logo Component', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Logo)
     expect(wrapper.isVueInstance()).toBeTruthy()
