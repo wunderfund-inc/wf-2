48ce472987aacc06cca361cf7c5efb3c0f89909b
Justin Chiou <chiou.kai@gmail.com>
2019-06-13 11:33:43 -0400

    write better test scaffold

--- components/ButtonLink/test.spec.js
+++ components/ButtonLink/test.spec.js
@@ -1,9 +1,38 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Component from './index'
+
+let wrapper
+
+const linkClass = 'button--green'
+const url = 'https://github.com/nuxt/nuxt.js'
+const title = 'Github'
+
+beforeEach(() => {
+  wrapper = shallowMount(Component, {
+    propsData: {
+      linkClasses: linkClass,
+      url: url,
+      title: title
+    }
+  })
+})
+
+afterEach(() => wrapper.destroy())
 
 describe('ButtonLink Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Component)
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('check all prop data', () => {
+    expect(wrapper.props('linkClasses')).toBe(linkClass)
+    expect(wrapper.props('url')).toBe(url)
+    expect(wrapper.props('title')).toBe(title)
+  })
+
+  test('rendering component with prop data', () => {
+    expect(wrapper.classes()).toContain(linkClass)
+    expect(wrapper.attributes('href')).toBe(url)
+    expect(wrapper.text()).toBe(title)
   })
 })
--- components/Logo/test.spec.js
+++ components/Logo/test.spec.js
@@ -1,9 +1,19 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Component from './index'
+
+let wrapper
+
+beforeEach(() => (wrapper = shallowMount(Component)))
+
+afterEach(() => wrapper.destroy())
 
 describe('Logo Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Component)
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('all classes are accounted for', () => {
+    expect(wrapper.classes()).toContain('VueToNuxtLogo')
+    expect(wrapper.contains('.VueToNuxtLogo')).toBe(true)
   })
 })
