32f2f56bd61445e22f31b9cf6cc7719d13605159
Justin Chiou <chiou.kai@gmail.com>
2019-08-29 11:03:02 -0400

    add google font

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,8 +19,8 @@ export default {
 
 <style lang="scss">
 html {
-  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
@@ -38,43 +38,43 @@ html {
     border-left: 0.5rem solid #c89f5c;
   }
   h1 {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 60px;
     font-weight: 300;
     color: #272727;
   }
   h2 {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 50px;
     font-weight: 200;
     color: #272727;
   }
   h3 {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 35px;
     font-weight: 400;
     color: #272727;
   }
   h4 {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 24px;
     font-weight: 400;
     color: #272727;
   }
   h5 {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 19px;
     font-weight: 300;
     color: #272727;
   }
   h6 {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 16px;
     font-weight: 300;
     color: #272727;
   }
   p {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     font-size: 12px;
     font-weight: 400;
     color: #272727;
@@ -84,7 +84,7 @@ html {
     color: white;
   }
   .form-control {
-    font-family: 'Montserrat';
+    font-family: "Montserrat";
     height: 50px;
     font-size: 14px;
     color: #494949;
@@ -124,7 +124,7 @@ html {
 @media (max-width: 540px) {
   html {
     h1 {
-      font-family: 'Montserrat';
+      font-family: "Montserrat";
       font-size: 30px;
     }
     h2 {
