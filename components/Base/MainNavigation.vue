22e4145d124f99530aa24ec9d7a148e3862c966c
Justin Chiou <chiou.kai@gmail.com>
2019-08-29 11:25:52 -0400

    create a generic navbar

--- components/Base/MainNavigation.vue
+++ components/Base/MainNavigation.vue
@@ -1,8 +1,13 @@
-<template>
-  <b-nav>
-    <b-nav-item active>Active</b-nav-item>
-    <b-nav-item>Link</b-nav-item>
-    <b-nav-item>Another Link</b-nav-item>
-    <b-nav-item disabled>Disabled</b-nav-item>
-  </b-nav>
+<template lang="pug">
+  b-navbar.pt-4(toggleable="sm" type="light" variant="transparent")
+    b-container
+      b-navbar-brand(href="#")
+        img.d-inline-block.align-middle(src="../../assets/logo.png" width="200")
+      b-navbar-toggle(target="collapse")
+      b-collapse#collapse(is-nav)
+        b-navbar-nav.ml-auto
+          b-nav-item Link
+          b-nav-item Link
+          b-nav-item Link
+          b-nav-item Link
 </template>
