363f814386ff363d3129cf658da86842b2ca402f
Justin Chiou <chiou.kai@gmail.com>
2019-08-30 16:22:24 -0400

    create a Campaign Progress Section component

---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div#campaign
+    section#hero
+    progress-section
+    section#content
+      b-container.py-4
+        b-row
+          b-col(md="9")
+            h1 Main content
+          b-col(md="3")
+            aside
+              h1 Sidebar
+</template>
+
+<script>
+import ProgressSection from "@/components/Campaign/ProgressSection";
+
+export default {
+  components: {
+    ProgressSection
+  }
+};
+</script>
