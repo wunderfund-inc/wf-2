a3a8fd9698234a69161d14a6d6a78e1147fc1c3d
Justin Chiou <chiou.kai@gmail.com>
2019-09-04 12:03:04 -0400

    make a section for faq navigation

---
+++ components/Faq/FaqNavbar/script.js
@@ -0,0 +1,7 @@
+export default {
+  data() {
+    return {
+      faqMenu: ["general", "investors", "issuers", "legal"]
+    };
+  }
+};
---
+++ pages/faq/_slug.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  main
+    b-container
+      section#faq-nav
+        b-row
+          b-col
+          b-col
+            faq-navbar
+          b-col
+</template>
+
+<script>
+import FaqNavbar from "@/components/Faq/FaqNavbar/index";
+
+export default {
+  components: {
+    FaqNavbar
+  },
+  data() {
+    return {
+      word: this.$route.params.slug
+    };
+  }
+};
+</script>
