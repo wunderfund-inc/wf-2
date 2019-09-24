6eb6d051e3824bbe539d59e86fd0956664a302bb
Justin Chiou <chiou.kai@gmail.com>
2019-09-24 13:33:08 -0400

    introduce the main button again!

---
+++ components/Common/MainButton/index.vue
@@ -0,0 +1,3 @@
+<template lang="pug" src="./template.pug" />
+<script src="./script.js" />
+<style lang="scss" src="./style.scss" scoped />
---
+++ components/Common/MainButton/script.js
@@ -0,0 +1,9 @@
+export default {
+  props: {
+    txt: {
+      type: String,
+      default: "",
+      required: true
+    }
+  }
+};
