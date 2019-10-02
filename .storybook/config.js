c62c863600661c0b66d0c07eb0891cd34b4a06d1
Justin Chiou <chiou.kai@gmail.com>
2019-10-02 14:27:10 -0400

    configure storybook to refax nuxt-link

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,20 @@
 import { configure } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.min.css";
 
 Vue.use(BootstrapVue);
-
+Vue.component("nuxt-link", {
+  props: ["to"],
+  methods: {
+    log() {
+      action("link target")(this.to)
+    },
+  },
+  template: `<a href="#" @click.prevent="log()"><slot>NuxtLink</slot></a>`,
+})
 const req = require.context("../components", true, /stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
