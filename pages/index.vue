71feb33a8267f23baf2d479b8c9a3d1189d9f3db
Justin Chiou <chiou.kai@gmail.com>
2019-10-04 10:30:34 -0400

    import Browse component to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,20 @@
   main
     hero-section
     email-capture
+    section-browse
+      h5.text-center.pb-4 Companies Currently Fundraising:
 </template>
 
 <script>
-import HeroSection from "@/components/Home/HeroSection/index";
-import EmailCapture from "@/components/Home/EmailCapture/index";
+import HeroSection from "@/components/Home/HeroSection";
+import EmailCapture from "@/components/Home/EmailCapture";
+import SectionBrowse from "@/components/Browse/SectionBrowse";
 
 export default {
   components: {
     HeroSection,
-    EmailCapture
+    EmailCapture,
+    SectionBrowse
   }
 };
 </script>
