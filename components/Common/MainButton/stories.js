7d7cd0395fbcff2a5960cab8d9239a791e2291f7
Justin Chiou <chiou.kai@gmail.com>
2019-10-04 09:39:45 -0400

    use addons for custom story display

--- components/Common/MainButton/stories.js
+++ components/Common/MainButton/stories.js
@@ -1,12 +1,21 @@
 import { storiesOf } from "@storybook/vue";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
 import MainButton from "./index.vue";
 
-storiesOf("Main Button", module).add("Normal text", () => ({
-  components: { MainButton },
-  template: `
-    <b-container class="py-5">
-      <main-button txt="Story Time!" />
-    </b-container>
-  `
-}));
+storiesOf("Main Button", module)
+  .addDecorator(withKnobs)
+  .add("Variable text input", () => ({
+    components: { MainButton },
+    props: {
+      text: {
+        type: String,
+        default: () => text("Text", "Story Time")
+      }
+    },
+    template: `
+      <b-container class="py-5">
+        <main-button>{{text}}</main-button>
+      </b-container>
+    `
+  }));
