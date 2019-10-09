cb6aebfde620c7ece6db3f0b12e23626e7bfa008
Justin Chiou <chiou.kai@gmail.com>
2019-10-09 10:40:06 -0400

    add manual login form

---
+++ components/Auth/LoginForm.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  b-form
+    b-form-group.text-left(
+      label="Email Address"
+      label-for="input-email"
+      :invalid-feedback="invalidFeedback"
+      :valid-feedback="validFeedback"
+      :state="state"
+    )
+      b-form-input#input-email(
+        type="email"
+        v-model="email"
+        :state="state"
+        trim
+      )
+    b-form-group.text-left(
+      label="Password"
+      label-for="input-password"
+      :invalid-feedback="invalidFeedback"
+      :valid-feedback="validFeedback"
+      :state="state"
+    )
+      b-form-input#input-password(
+        type="password"
+        v-model="password"
+        :state="state"
+        trim
+      )
+    button.btn.btn-lg.btn-primary.btn-block Sign in
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null
+    };
+  }
+};
+</script>
