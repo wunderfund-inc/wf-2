a7d5c60b573de2332b4700dc641009a2a4e71b25
Justin Chiou <chiou.kai@gmail.com>
2019-10-16 16:21:02 -0400

    use card-flushed list to enumerate investments

--- components/Account/AccountInvestmentList/index.vue
+++ components/Account/AccountInvestmentList/index.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
-  section
-    h1.py-3 My Investment Summaries:
-    account-investment-item
-    account-investment-item
-    account-investment-item
+  b-card(no-body)
+    b-card-body
+      h1.mb-0 My Investments:
+    b-list-group(flush)
+      b-list-group-item(
+        v-for="(investment, index) in 3"
+        :key="index"
+      )
+        account-investment-item
 </template>
 
 <script>
--- components/Account/AccountSpendLimitCard/index.vue
+++ components/Account/AccountSpendLimitCard/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  b-card.mb-4(no-body)
+  b-card.mb-3(no-body)
     b-card-body
       h1 Investment Limit: $2,200.00
       small.text-muted Based on your reported annual income and net worth.
