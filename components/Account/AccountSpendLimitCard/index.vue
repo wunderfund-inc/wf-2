e2d3ed2132e4c8050495cd9e489dace970864599
Justin Chiou <chiou.kai@gmail.com>
2019-10-17 11:19:31 -0400

    pass spend pool out as props

--- components/Account/AccountSpendLimitCard/index.vue
+++ components/Account/AccountSpendLimitCard/index.vue
@@ -1,9 +1,20 @@
 <template lang="pug">
   b-card.mb-3(no-body)
     b-card-body
-      h1 Investment Limit: $2,200.00
+      h1 Investment Limit: {{ spendPool | asCurrency }}
       small.text-muted Based on your reported annual income and net worth.
         |
         |
         a(href="#") Update it!
 </template>
+
+<script>
+export default {
+  props: {
+    spendPool: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
