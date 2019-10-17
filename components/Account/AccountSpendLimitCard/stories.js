e5a834f76be044a4f3037e257af4daed6d192637
Justin Chiou <chiou.kai@gmail.com>
2019-10-17 11:31:52 -0400

    add story for spend limit card

---
+++ components/Account/AccountSpendLimitCard/stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, number } from "@storybook/addon-knobs";
+
+import AccountSpendLimitCard from "./index.vue";
+
+storiesOf("Account Page Components", module)
+  .addDecorator(withKnobs)
+  .add("Spend Limit Card", () => ({
+    components: { AccountSpendLimitCard },
+    props: {
+      spendPool: {
+        type: Number,
+        default: () => number("Spend Pool", 2200)
+      }
+    },
+    template: `
+      <b-container class="py-5">
+        <b-row>
+          <b-col cols="9">
+            <account-spend-limit-card :spend-pool="spendPool" />
+          </b-col>
+        </b-row>
+      </b-container>
+    `
+  }));
