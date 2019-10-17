c36ffe53a2191c4c97c8f44c34a884d45b122085
Justin Chiou <chiou.kai@gmail.com>
2019-10-17 12:25:28 -0400

    add links and active classes to account sidebar

--- components/Account/AccountSidebar/index.vue
+++ components/Account/AccountSidebar/index.vue
@@ -1,7 +1,14 @@
-<template lang="pug">
+<template lang="pug" functional>
   b-card.mb-4(no-body)
     b-list-group(flush)
-      b-list-group-item Profile
-      b-list-group-item Accreditation
-      b-list-group-item Account
+      b-list-group-item(to="/u" exact-active-class="active") My Investments
+      b-list-group-item(to="/u/profile" exact-active-class="active") Profile
+      b-list-group-item(to="/u/accredit" exact-active-class="active") Accreditation
+      b-list-group-item(to="/u/account" exact-active-class="active") Account
 </template>
+
+<script>
+export default {
+  functional: true
+};
+</script>
