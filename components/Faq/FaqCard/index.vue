ac97f4d3a78a7d1657d1143e3f5a628e5cc697c0
Justin Chiou <chiou.kai@gmail.com>
2019-10-19 12:37:36 -0400

    componentize card from FAQ index page

---
+++ components/Faq/FaqCard/index.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  b-link.text-decoration-none(:to="`/faq/${faqlink}`")
+    b-card(
+      :img-src="imgSrc"
+      img-top
+      no-body
+      border-variant="transparent"
+      bg-variant="transparent"
+    )
+      b-card-footer(
+        footer-bg-variant="transparent"
+        footer-border-variant="transparent"
+      ) {{ faqLink | pluralFaq | properCase }}
+</template>
+
+<script>
+export default {
+  props: {
+    faqLink: {
+      type: String,
+      required: true
+    },
+    imgSrc: {
+      type: String,
+      required: false,
+      default:
+        "https://www.worldphoto.org/sites/default/files/styles/field_gallery_images-masonry/public/239874_253580_0_%20%C2%A9%20Mojtaba%20Nobakht%2C%20Iran%2C%20Commended%2C%20Open%20Competition%2C%20Portraits%2C%202017%20Sony%20World%20Photography%20Awards.jpg?itok=jN6XnQwD"
+    }
+  }
+};
+</script>
--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -3,24 +3,17 @@
     h1 Frequently Asked Questions (FAQs)
     b-row.py-5
       b-col.py-2(cols="12" md="3" v-for="link in links" :key="link.id")
-        b-link.text-decoration-none(:to="`/faq/${link}`")
-          b-card(
-            img-src="https://www.worldphoto.org/sites/default/files/styles/field_gallery_images-masonry/public/239874_253580_0_%20%C2%A9%20Mojtaba%20Nobakht%2C%20Iran%2C%20Commended%2C%20Open%20Competition%2C%20Portraits%2C%202017%20Sony%20World%20Photography%20Awards.jpg?itok=jN6XnQwD"
-            img-top
-            no-body
-            border-variant="transparent"
-            bg-variant="transparent"
-          )
-            b-card-footer(
-              footer-bg-variant="transparent"
-              footer-border-variant="transparent"
-            ) {{ link | pluralFaq | properCase }}
+        faq-card(:faq-link="link")
 </template>
 
 <script>
 import d from "./data.json";
+import FaqCard from "@/components/Faq/FaqCard";
 
 export default {
+  components: {
+    FaqCard
+  },
   data() {
     return {
       links: d.faqLinks
