2f2de871b10edb15fee5b705627143314616d6be
Justin Chiou <chiou.kai@gmail.com>
2019-11-04 09:44:50 -0500

    add conditional logic for entity option

--- components/Checkout/TransactionType/index.vue
+++ components/Checkout/TransactionType/index.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
   b-form-group(label="Which type of investment is this?")
-    b-form-radio(
-      value="PERSONAL" 
-      v-model="selectedType" 
-      name="selected-type"
-    ) A Personal investment.
-    b-form-radio(
-      value="ENTITY" 
-      v-model="selectedType" 
-      name="selected-type"
-    ) An investment on behalf an entity.
+    b-form-radio-group
+      b-form-radio(
+        value="PERSONAL" 
+        v-model="selectedType" 
+        name="selected-type"
+      ) A Personal investment.
+      b-form-radio(
+        value="ENTITY" 
+        v-model="selectedType" 
+        name="selected-type"
+        :disabled="!hasEntities"
+      ) An investment on behalf an Entity.
+        br(v-if="!hasEntities")
+        small.text-muted(v-if="!hasEntities") You don't have any entities filed. Apply Here!
 </template>
 
 <script>
@@ -22,6 +26,10 @@ export default {
       set(val) {
         this.$store.commit("checkout/SET_TRANSACTION_TYPE", val);
       }
+    },
+    hasEntities() {
+      return false;
+      // return this.$store.getters["user/hasEntities"];
     }
   }
 };
