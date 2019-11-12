6800daed5ef66001091f9a848e3684f756ba9585
Justin Chiou <chiou.kai@gmail.com>
2019-11-12 16:03:40 -0500

    fix authentication middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,19 @@
-export default function({ store, redirect }) {
-  // If the user is not authenticated
-  if (!store.getters["auth/isAuthenticated"]) {
+export default function({ store, redirect, route }) {
+  if (isAuthenticated && atAuthRoute(route)) {
+    return redirect("/u");
+  } else if (!isAuthenticated && atUserRoute(route)) {
     return redirect("/auth/login");
   }
+}
+
+function isAuthenticated(store) {
+  return store.getters["auth/isAuthenticated"];
+}
+
+function atUserRoute(route) {
+  return route.matched.some(record => record.path === "/u");
+}
+
+function atAuthRoute(route) {
+  return route.matched.some(record => record.path === "/auth");
 }
