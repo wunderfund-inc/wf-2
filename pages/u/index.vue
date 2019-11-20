759e4117af17eef868ef937e38e9d934bb8d964a
Justin Chiou <chiou.kai@gmail.com>
2019-11-20 10:48:42 -0500

    start a section on Entities Filed

--- pages/u/index.vue
+++ pages/u/index.vue
@@ -6,11 +6,13 @@
         :spend-max="2200"
         :is-accredited="false"
       )
+      account-entity-list(:entities="entities")
       account-investment-list(:investments="investments")
 </template>
 
 <script>
 import AccountProfile from "@/components/Account/AccountProfile";
+import AccountEntityList from "@/components/Account/AccountEntityList";
 import AccountInvestmentList from "@/components/Account/AccountInvestmentList";
 
 // TODO: remove when we have a Store Action for this.
@@ -20,11 +22,13 @@ export default {
   middleware: "authenticated",
   components: {
     AccountProfile,
+    AccountEntityList,
     AccountInvestmentList
   },
   data() {
     return {
-      investments: d.investments
+      investments: d.investments,
+      entities: d.entities
     };
   }
 };
