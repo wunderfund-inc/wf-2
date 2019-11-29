99d245a0367efd6a79b6039e4d04a90f59f7fa53
Justin Chiou <chiou.kai@gmail.com>
2019-11-29 11:46:12 -0500

    make State Inputs a dropdown

--- components/Form/Entity/EntityAddress/StateInput/index.vue
+++ components/Form/Entity/EntityAddress/StateInput/index.vue
@@ -1,13 +1,19 @@
 <template lang="pug">
-  b-form-group(
-    label="State"
-    label-for="entity-state"
-  )
-    b-form-input#entity-state(v-model="state" trim)
+  b-form-group(label="State")
+    b-form-select(v-model="state" :options="states")
+      template(v-slot:first)
+        option(:value="null" disabled) -- Please Select --
 </template>
 
 <script>
+import d from "@/components/Form/data.json";
+
 export default {
+  data() {
+    return {
+      states: d.states
+    };
+  },
   computed: {
     state: {
       get() {
