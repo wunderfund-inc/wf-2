90af5768e3691ef748de3316893a34a240dbd9db
Justin Chiou <chiou.kai@gmail.com>
2020-01-20 13:01:45 -0500

    route links to offering id

--- pages/c/_companyId/index.vue
+++ pages/c/_companyId/index.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script>
+import { db } from "@/plugins/firebase";
 import CampaignContent from "@/components/Campaign/CampaignContent";
 
 export default {
@@ -12,6 +13,20 @@ export default {
   },
   async fetch({ store, params }) {
     try {
+      const authData = store.getters["auth/currentUserAuth"];
+      const uid = authData.uid || authData.user_id;
+      const user = await db
+        .collection("users")
+        .doc(uid)
+        .get();
+      const userData = user.data();
+      await store.dispatch("user/setCurrentUser", userData);
+      await store.dispatch("user/setProfileNameAttribute", {
+        ...userData.name
+      });
+      await store.dispatch("user/setProfileAddressAttribute", {
+        ...userData.address
+      });
       await store.dispatch("company/fetchCompany", params.companyId);
     } catch (error) {
       // eslint-disable-next-line
