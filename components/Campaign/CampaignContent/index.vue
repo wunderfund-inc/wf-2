d34df048333c49ddc2afd610fa468ece3824d20d
Justin Chiou <chiou.kai@gmail.com>
2020-01-29 09:53:21 -0500

    format campaign page a bit

--- components/Campaign/CampaignContent/index.vue
+++ components/Campaign/CampaignContent/index.vue
@@ -13,10 +13,10 @@
                   allowfullscreen
                 )
             .row
-              .col.text-right
+              .col.pt-2
                 small.px-1(v-if="company.website")
                   a.text-muted(:href="company.website" target="_blank")
-                    font-awesome-icon(:icon="['fas', 'link']")
+                    font-awesome-icon.fa-lg(:icon="['fas', 'link']")
                 small.px-1(
                   v-for="(link, index) in company.socialMediaLinks"
                   :key="index"
@@ -25,11 +25,11 @@
                     :href="`${link.platform}/${link.handle}`"
                     target="_blank"
                   )
-                    font-awesome-icon(:icon="['fab', iconIt(link.platform)]")
+                    font-awesome-icon.fa-lg(:icon="['fab', iconIt(link.platform)]")
           .col-12.col-md-5
             h1 {{ company.name.short }}
             h4 {{ company.motto }}
-            h6.text-muted
+            small.text-muted
               font-awesome-icon(:icon="['fas', 'map-marker-alt']")
               |
               | {{ company.location.city }}, {{ company.location.state }}
@@ -48,22 +48,21 @@
                       span Raising: {{ offering.goal.min | currencyDisplayFormat }} - {{ offering.goal.max | currencyDisplayFormat }}
                       br
                       span Days Left: {{ offering.date.end | timeDistance }}
-            p Share this offering with your friends!
-            p
-              a.pr-3.text-muted
-                font-awesome-icon.fa-2x(:icon="['fab', 'facebook-square']")
+            small Share this offering with your friends!
               a.px-3.text-muted
-                font-awesome-icon.fa-2x(:icon="['fab', 'linkedin']")
+                font-awesome-icon.fa-lg(:icon="['fab', 'facebook-square']")
+              a.px-3.text-muted
+                font-awesome-icon.fa-lg(:icon="['fab', 'linkedin']")
               a.pl-3.text-muted
-                font-awesome-icon.fa-2x(:icon="['fab', 'twitter-square']")
+                font-awesome-icon.fa-lg(:icon="['fab', 'twitter-square']")
     section#metrics
       .container
         .row.text-center(v-if="company.offerings.length > 0")
           b-col.py-4(sm="4")
-            h4 Amount Raised:
+            h5 Amount Raised:
             span {{ amountRaised | asCurrency }}
           b-col.py-4(sm="4")
-            h4 Investments:
+            h5 Investments:
             span {{ company.investments.length }}
           b-col.py-4(sm="4")
             b-button.mt-2(
@@ -92,40 +91,39 @@
           .col-12.col-md-1
           .col
             .text-center
-              h1.pb-3 The Pitch
+            h1.pb-3 The Pitch
             div(v-html="company.otherContent")
           .col-12.col-md-1
         .row.pt-5
+          .col-12.col-md-1
           .col
-            .text-center
-              h1 The Team
-            .row.py-3(v-for="(employee, idx) in company.employees" :key="idx")
-              .col-12.col-md-3
-              .col.text-right
-                b-img(
-                  thumbnail
-                  :src="employee.image.url"
-                  style="max-height: 100px; max-width: 100px"
-                )
-              .col
-                h4.mt-2 {{ employee.name }}
-                p {{ employee.title }}
-              .col-12.col-md-3
-        .row.pt-5
-          .col
-            .text-center
-              h1.pb-3 FAQs
+            h1.pb-3 The Team
             .row
-              .col-12.col-md-4
-              .col-12.col-md-4
-                details(v-for="(faq, index) in company.faqs" :key="index")
-                  summary {{ faq.question }}
-                  p {{ faq.answer }}
-              .col-12.col-md-4
-        .row.py-5
+              .col-12.col-md-3.col-sm-6.py-3(v-for="(employee, idx) in company.employees" :key="idx")
+                b-card(no-body)
+                  .text-center.py-3
+                    b-img(
+                      thumbnail
+                      :src="employee.image.url"
+                      style="max-height: 100px; max-width: 100px"
+                    )
+                    b-card-text.pt-3
+                      h6 {{ employee.name }}
+                      small.mb-0 {{ employee.title }}
+          .col-12.col-md-1
+        .row.pt-5
+          .col-12.col-md-1
           .col
-            .text-center
-              h1 Questions?
+            h1.pb-3 FAQs
+            details(v-for="(faq, index) in company.faqs" :key="index")
+              summary {{ faq.question }}
+              p {{ faq.answer }}
+          .col-12.col-md-1
+        .row.py-5
+          .col-12.col-md-1
+          .col
+            h1 Questions?
+          .col-12.col-md-1
 </template>
 
 <script>
