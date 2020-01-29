56d0af55d64318eb3b2ddaa44486ce82a9635461
Justin Chiou <chiou.kai@gmail.com>
2020-01-29 18:49:01 -0500

    refax investment items

--- components/Account/AccountInvestmentItem/index.vue
+++ components/Account/AccountInvestmentItem/index.vue
@@ -9,40 +9,40 @@ aside
       b-col(cols="3").text-right
         b-button(
           variant="outline-secondary"
-          v-b-toggle="`investment-${investment.company.uid}`"
+          v-b-toggle="`investment-${investment.uid}`"
           size="sm"
         )
           span.when-opened Close
           span.when-closed Expand
-  b-collapse(:id="`investment-${investment.company.uid}`" accordion="investments")
+  b-collapse(:id="`investment-${investment.uid}`" accordion="investments")
     .row
       .col
         h4.mb-0.py-4 The Offering
     .row
       .col-12.col-sm-4
         h5 Raised:
-        h6 {{ amountRaised | asCurrency }}
+        h6 {{ totalRaised | asCurrency }}
       .col-12.col-sm-4
         h5 Goal:
-        h6 {{ investment.offering.goal.min | asCurrency }}
+        h6 {{ investment.offering.goal.min | currencyDisplayFormat }} - {{ investment.offering.goal.max | currencyDisplayFormat }}
       .col-12.col-sm-4
         h5 Status:
-        h6 8 days left!
+        h6 {{ investment.offering.date.end | timeDistance }} left
     .row
       .col
         h4.mb-0.py-4 Your Investment
     .row
       .col-12.col-sm-4
-        h5 Amount Invested:
-        h6 {{ investment.amount | asCurrency }}
+        h5 Amount Invested: #[small.text-success {{ investment.amount | asCurrency }}]
+        h5 Transaction Method: #[small.text-success {{ investment.method }}]
       .col-12.col-sm-4(v-if="investment.offering.securityType === 'EQUITY'")
         h5 Ownership (units):
         h6 {{ investment.amount / investment.offering.equity.pricePerShare }}
       .col-12.col-sm-4(v-if="investment.offering.securityType === 'EQUITY'")
         h5 Ownership (%):
         h6 {{ ((investment.amount / investment.offering.equity.pricePerShare)/investment.offering.equity.numberOfSecurities).toFixed(4) }}
-    b-row.py-4
-      b-col
+    .row.py-4
+      .col
         b-button(size="lg") View Agreement
 </template>
 
@@ -59,28 +59,24 @@ export default {
   },
   data() {
     return {
-      amountRaised: null
+      totalRaised: null
     };
   },
   async created() {
     try {
-      const allInvestments = this.investment.offering.investments;
-      const amounts = await Promise.all(
-        allInvestments.map(async investment => {
-          try {
-            const investmentRef = await db
-              .collection("investments")
-              .doc(investment)
-              .get();
-            const investmentData = investmentRef.data();
-            return investmentData.amount;
-          } catch (error) {
-            // eslint-disable-next-line
-            console.error(error);
-          }
-        })
-      );
-      this.amountRaised = amounts.reduce((total, val) => total + val);
+      const offeringId = this.investment.offeringId;
+
+      const querySnapshot = await db
+        .collection(`offerings/${offeringId}/investments`)
+        .get();
+
+      const investments = querySnapshot.docs.map(investment => {
+        return investment.data();
+      });
+
+      const amounts = investments.map(investment => investment.amount);
+
+      this.totalRaised = amounts.reduce((total, val) => total + val);
     } catch (error) {
       // eslint-disable-next-line
       console.error(error);
