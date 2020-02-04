fab9fed71fa714c7f04cff1902f87c594738f2ef
Justin Chiou <chiou.kai@gmail.com>
2020-02-04 11:38:43 -0500

    fix email CTA to use vuelidate flow

--- components/Home/EmailCapture/index.vue
+++ components/Home/EmailCapture/index.vue
@@ -11,52 +11,60 @@ section.py-5.cta__section
           b-form(v-if="show" @submit.stop.prevent="subscribeUser")
             b-input-group
               b-form-input(
-                v-model="email"
-                type="email"
                 required
+                type="email"
                 placeholder="Your email address"
+                v-model="$v.form.email.$model"
+                :state="validateState('email')"
               )
               b-input-group-append
-                b-button.color__gold(size="sm" @click="subscribeUser") Sign Up
+                b-button.color__gold(
+                  size="sm"
+                  @click="subscribeUser"
+                  :disabled="!$v.form.email.$dirty || $v.form.email.$invalid"
+                ) Sign Up
           p.text-center(v-else) You have subscribed to our newsletter. Thanks!
-        .text-center(v-if="error")
-          small.text-danger {{ error }}
+        .text-center(v-if="$v.form.email.$error")
+          small.text-danger Invalid email address.
       b-col.d-none.d-md-block
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
 import { email } from "vuelidate/lib/validators";
 
 export default {
+  mixins: [validationMixin],
   data() {
     return {
-      email: null,
-      show: true,
-      error: null
+      form: {
+        email: null
+      },
+      show: true
     };
   },
   validations: {
-    email: {
-      email
+    form: {
+      email: {
+        email
+      }
     }
   },
   methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+      return $dirty ? !$error : null;
+    },
     async subscribeUser() {
-      if (!this.$v.$invalid) {
-        try {
-          const url =
-            "https://us-central1-wunderfund-server.cloudfunctions.net/newsletterOnSubscribe";
-          await this.$axios.$post(url, { email: this.email });
-          this.error = null;
-          this.show = !this.show;
-          this.email = null;
-        } catch (error) {
-          // eslint-disable-next-line
-          console.error(error);
-          this.error = error;
-        }
-      } else {
-        this.error = "Error: please input a valid email address.";
+      try {
+        const url =
+          "https://us-central1-wunderfund-server.cloudfunctions.net/newsletterOnSubscribe";
+        await this.$axios.$post(url, { email: this.$v.email.$model });
+        this.show = !this.show;
+      } catch (error) {
+        // eslint-disable-next-line
+        console.error(error);
+        this.error = error;
       }
     }
   }
