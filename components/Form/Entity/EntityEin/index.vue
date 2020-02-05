919f8ceadd278572f4ef13976878578e9b23ef4d
Justin Chiou <chiou.kai@gmail.com>
2020-02-05 18:01:23 -0500

    validate EIN inputs

--- components/Form/Entity/EntityEin/index.vue
+++ components/Form/Entity/EntityEin/index.vue
@@ -3,11 +3,28 @@
     label="Employer Identification Number"
     label-for="entity-ein"
   )
-    b-form-input#entity-ein(v-model.number="entityEIN" trim)
+    b-form-input#entity-ein(v-model="entityEIN")
+    small.text-danger(
+      v-if="$v.entityEIN.$dirty && $v.entityEIN.$anyError"
+    ) - Invalid EIN
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required } from "vuelidate/lib/validators";
+
+const einRegex = /^(0[1-6]||1[0-6]|2[0-7]|[345]\d|[68][0-8]|7[1-7]|9[0-58-9])-?\d{7}$/;
+
 export default {
+  mixins: [validationMixin],
+  validations: {
+    entityEIN: {
+      required,
+      validRegex(ein) {
+        return einRegex.test(ein);
+      }
+    }
+  },
   computed: {
     entityEIN: {
       get() {
@@ -15,11 +32,14 @@ export default {
       },
       set(val) {
         this.$store.commit("user/SET_ENTITY_FORM_ATTRIBUTE", { ein: val });
+        this.$v.entityEIN.$touch();
       }
-    },
+    }
+  },
+  methods: {
     validEIN() {
-      const reg = /^(0[1-6]||1[0-6]|2[0-7]|[345]\d|[68][0-8]|7[1-7]|9[0-58-9])-?\d{7}$/;
-      return reg.test(this.entityEIN);
+      const { $dirty, $error } = this.$v.entityEIN;
+      return $dirty ? !$error : null;
     }
   }
 };
