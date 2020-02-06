2052ef041920332b6d6f8fb854a03e189d8ac25b
Justin Chiou <chiou.kai@gmail.com>
2020-02-06 08:40:31 -0500

    add spinner to login button

--- components/Form/Auth/LoginForm.vue
+++ components/Form/Auth/LoginForm.vue
@@ -1,32 +1,40 @@
 <template lang="pug">
-  b-form#via-password(@submit.stop.prevent="submitLogin")
+  b-form#via-password(
+    @submit.prevent="submitLogin"
+    @keypress.enter.prevent="submitLogin"
+  )
     b-form-group.text-left(
       label="Email Address"
       label-for="input-email"
-      :invalid-feedback="invalidFeedback"
-      :valid-feedback="validFeedback"
-      :state="state"
     )
       b-form-input#input-email(
         type="email"
         v-model="email"
-        :state="state"
         trim
       )
     b-form-group.text-left(
       label="Password"
       label-for="input-password"
-      :invalid-feedback="invalidFeedback"
-      :valid-feedback="validFeedback"
-      :state="state"
     )
       b-form-input#input-password(
         type="password"
         v-model="password"
-        :state="state"
         trim
       )
-    b-button(size="lg" variant="primary" block type="submit") Login
+    b-button(
+      type="submit"
+      variant="primary"
+      block
+      :disabled="submitting"
+    )
+      span.spinner-border.spinner-border-sm.mr-2(
+        v-if="submitting"
+        role="status"
+        aria-hiden="true"
+        style="margin-bottom: 4px"
+      )
+      span(v-if="submitting") Logging In...
+      span(v-if="!submitting") Sign Up
     p.text-danger.pt-3 {{ error }}
 </template>
 
@@ -36,15 +44,17 @@ export default {
     return {
       email: null,
       password: null,
-      invalidFeedback: null,
-      validFeedback: null,
-      state: null,
-      error: null
+      error: null,
+      submitting: false
     };
   },
   methods: {
+    toggleSubmission() {
+      this.submitting = !this.submitting;
+    },
     async submitLogin() {
       try {
+        this.submitting = true;
         await this.$store.dispatch("auth/loginUser", {
           email: this.email,
           password: this.password
@@ -55,7 +65,8 @@ export default {
       } catch (error) {
         // eslint-disable-next-line
         console.error(error);
-        this.error = error.message;
+        this.submitting = false;
+        this.error = error;
       }
     }
   }
