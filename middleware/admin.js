f643cce88ce52aa9176acc34bb12531fbfbd8676
Justin Chiou <chiou.kai@gmail.com>
2020-02-11 11:35:15 -0500

    create a guard route and for a created admin page

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,7 +1,13 @@
 import { auth } from "@/plugins/firebase";
 import { getUserFromCookie } from "@/helpers";
 
+const masterEmailList = [
+  "chiou.kai@gmail.com",
+  "justin@wunderfund.co",
+  "marvin@wunderfund.co"
+];
+
 export default function({ redirect, req }) {
   const user = process.server ? getUserFromCookie(req) : auth.currentUser;
-  if (!user) redirect("/auth/login");
+  if (!masterEmailList.includes(user.email)) redirect("/u");
 }
