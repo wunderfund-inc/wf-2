b749a7b736a07a2b5778afe06828f4686dda237d
Justin Chiou <chiou.kai@gmail.com>
2020-02-25 09:51:57 -0500

    refax checkout agreement logic

--- components/Form/Checkout/TransactionAgreements/index.vue
+++ components/Form/Checkout/TransactionAgreements/index.vue
@@ -3,11 +3,11 @@
     b-form-checkbox.py-2(
       v-for="(agreement, index) in agreements"
       :key="index"
-      :value="agreement.text"
+      :value="agreement"
       v-model="agreedTo"
       switch
     )
-      span(:class="agreedTo[index] ? 'text-muted' : ''") {{ agreement.text }}
+      span(:class="agreedTo.includes(agreement) ? 'text-muted' : ''") {{ agreement }}
 </template>
 
 <script>
@@ -15,31 +15,11 @@ export default {
   data() {
     return {
       agreements: [
-        {
-          text:
-            "I understand that startups and small businesses are very risky. I acknowledge a potential 100% loss on all investments into this particular company, using Wunderfund's technology.",
-          value: true
-        },
-        {
-          text:
-            "I understand securities created via Wunderfund's technology are not easily resold. There is no secondary market for these particular securities offered.",
-          value: true
-        },
-        {
-          text:
-            "I are savvy enough to understand the subject of crowdfunding. I have contacted the company (or posted in the comments!) with any questions I may have before starting this commitment process.",
-          value: true
-        },
-        {
-          text:
-            "I understand that this platform (and the team behind it) cannot offer investment advice. I must have the sophistication and education necessary to evaluate investments on my own.",
-          value: true
-        },
-        {
-          text:
-            "I acknowledge that the information regarding my identity and/or entity I represent is true to my knowledge.",
-          value: true
-        }
+        "I understand that startups and small businesses are very risky. I acknowledge a potential 100% loss on all investments into this particular company, using Wunderfund's technology.",
+        "I understand securities created via Wunderfund's technology are not easily resold. There is no secondary market for these particular securities offered.",
+        "I are savvy enough to understand the subject of crowdfunding. I have contacted the company (or posted in the comments!) with any questions I may have before starting this commitment process.",
+        "I understand that this platform (and the team behind it) cannot offer investment advice. I must have the sophistication and education necessary to evaluate investments on my own.",
+        "I acknowledge that the information regarding my identity and/or entity I represent is true to my knowledge."
       ]
     };
   },
