8d477c83fe40756b8f5ad449db7defbe31aaed2c
Justin Chiou <chiou.kai@gmail.com>
2020-03-25 15:34:59 -0400

    remove storybook

--- .storybook/plugins/bootstrap-vue.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-vue/dist/bootstrap-vue.min.css";
-
-Vue.use(BootstrapVue);
