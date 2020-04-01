e048099a4c7f0bd1ded74f63bfa5b1c04267bb0c
Justin Chiou <chiou.kai@gmail.com>
2020-04-01 15:11:56 -0400

    remove more stories

--- components/RaiseCapital/SectionJumbotron/stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/vue";
-
-import Component from "./index.vue";
-
-storiesOf("Page - Raise Capital", module).add("Section - Jumbotron", () => ({
-  components: { Component },
-  template: `<component />`
-}));
