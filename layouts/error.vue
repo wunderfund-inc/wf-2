9a75ba971ad915c4b050bcfd184947ff4bbfef87
Justin Chiou <chiou.kai@gmail.com>
2020-04-06 14:23:03 -0400

    add generic error page

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <b-img src="~assets/bg/404.jpg" class="fill-page" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default() {},
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.fill-page {
+  width: 100vw;
+}
+</style>
