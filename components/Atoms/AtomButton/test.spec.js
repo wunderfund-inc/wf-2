7a81ca0e87034d67524effeddd8b70b6ae4c6b1c
Justin Chiou <chiou.kai@gmail.com>
2020-07-10 15:51:49 -0400

    remove atoms and molecules

--- components/Atoms/AtomButton/test.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import Component from "./index.vue";
-
-test("Internal Link Component", () => {
-  const text = "Submit";
-  const wrapper = shallowMount(Component, { propsData: { text } });
-  expect(wrapper.classes()).toContain("btn");
-  expect(wrapper.text()).toBe(text);
-});
--- components/Molecules/CompanyLink/test.spec.js
+++
@@ -1,8 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import Component from "./index.vue";
-
-test("Internal Link Component", () => {
-  const wrapper = shallowMount(Component);
-  expect(wrapper.attributes("to")).toBe("/");
-  expect(wrapper.attributes("text")).toBe("Wunderfund.co");
-});
