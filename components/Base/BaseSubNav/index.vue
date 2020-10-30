17ecf6c0d06925bafc5d2a88443803407654ab71
Justin Chiou <chiou.kai@gmail.com>
2020-10-30 14:08:53 -0400

    refactor: redo FAQ pages

--- components/Base/BaseSubNav/index.vue
+++ components/Base/BaseSubNav/index.vue
@@ -23,7 +23,7 @@ export default {
   },
   computed: {
     color() {
-      switch (process.env.PLATFORM) {
+      switch (this.$config.PLATFORM) {
         case "WFP":
           return "success";
         case "WFH":
