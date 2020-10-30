3bdaacdf4e40877c5870ae2269422b4fea0caf7c
Justin Chiou <chiou.kai@gmail.com>
2020-10-30 13:43:10 -0400

    refactor: remove imports

--- pages/raise-capital.vue
+++ pages/raise-capital.vue
@@ -1,27 +1,9 @@
 <template>
   <main>
-    <section-jumbotron />
-    <section-unique />
-    <section-ops />
-    <section-eligibility />
-    <section-faq />
+    <SectionJumbotron />
+    <SectionUnique />
+    <SectionOps />
+    <SectionEligibility />
+    <SectionFaq />
   </main>
 </template>
-
-<script>
-import SectionJumbotron from "@/components/RaiseCapital/SectionJumbotron";
-import SectionUnique from "@/components/RaiseCapital/SectionUnique";
-import SectionOps from "@/components/RaiseCapital/SectionOps";
-import SectionEligibility from "@/components/RaiseCapital/SectionEligibility";
-import SectionFaq from "@/components/RaiseCapital/SectionFaq";
-
-export default {
-  components: {
-    SectionJumbotron,
-    SectionUnique,
-    SectionOps,
-    SectionEligibility,
-    SectionFaq,
-  },
-};
-</script>
