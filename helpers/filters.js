4c0e128afe3474df4de28d668f8a6513aa39b1a6
Justin Chiou <chiou.kai@gmail.com>
2022-03-31 17:55:03 -0400

    fix: lint yet again

--- helpers/filters.js
+++ helpers/filters.js
@@ -96,7 +96,7 @@ export const timeDistance = (val) => {
   return formatDistance(
     new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
     new Date(`${val} 23:59:59`).toLocaleString("en-US", {
-      timeZone: "America/New_York"
+      timeZone: "America/New_York",
     })
   );
 };
