495874b80e2bc5f82f9472f8a4384e9b3e102dcf
Justin Chiou <chiou.kai@gmail.com>
2022-03-31 17:52:27 -0400

    fix: lint again

--- helpers/filters.js
+++ helpers/filters.js
@@ -95,7 +95,9 @@ Vue.filter("einStrFormat", einNumToStr);
 export const timeDistance = (val) => {
   return formatDistance(
     new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
-    new Date(`${val} 23:59:59`).toLocaleString("en-US", { timeZone: "America/New_York" })
+    new Date(`${val} 23:59:59`).toLocaleString("en-US", {
+      timeZone: "America/New_York"
+    })
   );
 };
 Vue.filter("timeDistance", timeDistance);
