f4310c483f3a288127a0c4122a3de6df50883267
Justin Chiou <chiou.kai@gmail.com>
2022-03-31 17:45:12 -0400

    fix: timezone-aware time distance

--- helpers/filters.js
+++ helpers/filters.js
@@ -93,7 +93,10 @@ Vue.filter("einStrFormat", einNumToStr);
  * Function to calculate days left
  */
 export const timeDistance = (val) => {
-  return formatDistance(new Date(), new Date(`${val} 23:59:59`));
+  return formatDistance(
+    new Date().toLocaleString("en-US", {timeZone: "America/New_York"}), 
+    new Date(`${val} 23:59:59`).toLocaleString("en-US", {timeZone: "America/New_York"})
+  );
 };
 Vue.filter("timeDistance", timeDistance);
 
