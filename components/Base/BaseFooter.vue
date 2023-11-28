70388381ed063b66fe0f71615fbd29f7c6c696ac
Justin Chiou <chiou.kai@gmail.com>
2023-11-28 13:41:40 -0500

    fix: disclosure in footer

--- components/Base/BaseFooter.vue
+++ components/Base/BaseFooter.vue
@@ -44,43 +44,85 @@
         </div>
         <div class="col-12 col-md-9 pb-5">
           <p>
-            <PlatformLink class="text-light font-weight-bold" />
-            is run by Wunderfund, Inc. By using
-            <PlatformLink class="text-light font-weight-bold" />
-            you accept our
-            <nuxt-link
-              to="/legal/terms-of-use"
-              class="text-light font-weight-bold"
-            >
-              Terms of Use
-            </nuxt-link>
-            and
-            <nuxt-link
-              to="/legal/privacy-policy"
-              class="text-light font-weight-bold"
-            >
-              Privacy Policy.
-            </nuxt-link>
-            By choosing to invest using
-            <PlatformLink class="text-light font-weight-bold" />
-            you accept our
-            <nuxt-link
-              to="/legal/investor-agreement"
-              class="text-light font-weight-bold"
-            >
-              Investor Agreement.
-            </nuxt-link>
-            By choosing to post your offering using
-            <PlatformLink class="text-light font-weight-bold" />
-            technologies, you accept our
-            <nuxt-link
-              to="/legal/startup-agreement"
-              class="text-light font-weight-bold"
-            >
-              Startup Agreement.
-            </nuxt-link>
-            <PlatformLink class="text-light font-weight-bold" />
-            is registered with the SEC and is a member of FINRA.
+            This site (the "Site") is owned and maintained by Wunderfund, Inc.,
+            which is not a registered broker-dealer. Wunderfund, Inc. does not
+            give investment advice, endorsement, analysis or recommendations
+            with respect to any securities. All securities listed here are being
+            offered by, and all information included on this Site is the
+            responsibility of, the applicable issuer of such securities. The
+            intermediary facilitating the offering will be identified in such
+            offering's documentation.
+          </p>
+          <p>
+            All funding-portal activities are conducted by Wunderfund, Inc., a
+            funding portal which is registered with the US Securities and
+            Exchange Commission (SEC) as a funding portal (Portal) and is a
+            member of the Financial Industry Regulatory Authority (FINRA).
+            Please check our background on
+            <FooterLink
+              :on-platform="false"
+              link="https://www.finra.org/about/firms-we-regulate/funding-portals-we-regulate"
+              title="FINRA's Funding Portal Page."
+            />
+          </p>
+          <p>
+            Wunderfund, Inc., does not make investment recommendations and no
+            communication, through this Site or in any other medium should be
+            construed as a recommendation for any security offered on or off
+            this investment platform. Investment opportunities posted on this
+            Site are private placements of securities that are not publicly
+            traded, involve a high degree of risk, may lose value, are subject
+            to holding period requirements and are intended for investors who do
+            not need a liquid investment. Past performance is not indicative of
+            future results. Investors must be able to afford the loss of their
+            entire investment.
+          </p>
+          <p>
+            Wunderfund, Inc., and any of its officers, directors, agents and
+            employees makes any warranty, express or implied, of any kind
+            whatsoever related to the adequacy, accuracy or completeness of any
+            information on this Site or the use of information on this site.
+            Offers to sell securities can only be made through official offering
+            documents that contain important information about the investment
+            and the issuers, including risks. Investors should carefully read
+            the offering documents. Investors should conduct their own due
+            diligence and are encouraged to consult with their tax, legal and
+            financial advisors.
+          </p>
+          <p>
+            By accessing the Site and any pages thereof, you agree to be bound
+            by the Wunderfund Portal's
+            <FooterLink link="/legal/terms-of-use" title="Terms of Use" /> and
+            <FooterLink link="/legal/privacy-policy" title="Privacy Policy" />.
+          </p>
+          <p>
+            The inclusion or exclusion of an issuer on the Platform Page and/or
+            Wunderfund's Homepage, which includes offerings conducted under
+            regulation crowdfunding as well as other exemptions from
+            registration, is not based upon any endorsement or recommendation by
+            Wunderfund, Inc., nor any of its affiliates, officers, directors,
+            agents, and employees. Rather, issuers of securities may, in their
+            sole discretion, opt-out of being listed on the Platform Page and
+            Homepage.
+          </p>
+          <p>
+            Investors should verify any issuer information they consider
+            important before making an investment.
+          </p>
+          <p>
+            Investments in private companies are particularly risky and may
+            result in a total loss of invested capital. Past performance of a
+            security or a company does not guarantee future results or returns.
+            Only investors who understand the risks of early-stage investment
+            and who meet the Wunderfund&#39;s investment criteria may invest.
+          </p>
+          <p>
+            Wunderfund Inc., cannot verify information provided by companies on
+            this Site and makes no assurance as to the completeness or accuracy
+            of any such information. Additional information about companies
+            fundraising on the Site can be found by searching the EDGAR
+            database, or the offering documentation located on the Site when the
+            offering does not require an EDGAR filing.
           </p>
         </div>
       </div>
---
+++ components/FooterLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <span>
+    <nuxt-link v-if="onPlatform" :to="link" class="text-light font-weight-bold">
+      <span>{{ title }}</span>
+    </nuxt-link>
+    <a v-else :href="link" target="_blank" class="text-light font-weight-bold">
+      <span>{{ title }}</span>
+    </a>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "FooterLink",
+  props: {
+    onPlatform: {
+      type: Boolean,
+      default: true,
+    },
+    link: {
+      type: String,
+      default: "/",
+    },
+    title: {
+      type: String,
+      default: "Wunderfund.co",
+    },
+  },
+};
+</script>
