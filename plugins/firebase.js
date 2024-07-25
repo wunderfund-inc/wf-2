6bcd90052bc13559f117d8c62466320651ce6b85
Justin Chiou <chiou.kai@gmail.com>
2024-07-24 21:53:59 -0400

    refax: firebase using v9

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,51 +1,49 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/functions";
-import "firebase/storage";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {
+  FacebookAuthProvider,
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, serverTimestamp } from "firebase/firestore/lite";
+import { getFunctions } from "firebase/functions";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBOYoxaEM-K8MLGuzPQ68z3rR2d7Hb1v6U",
-    authDomain: "wunderfund-server.firebaseapp.com",
-    databaseURL: "https://wunderfund-server.firebaseio.com",
-    projectId: "wunderfund-server",
-    storageBucket: "wunderfund-server.appspot.com",
-    messagingSenderId: "222886391134",
-    appId: "1:222886391134:web:82469ba2bcaf85037f33cf",
-  };
-  firebase.initializeApp(firebaseConfig);
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyBOYoxaEM-K8MLGuzPQ68z3rR2d7Hb1v6U",
+  authDomain: "wunderfund-server.firebaseapp.com",
+  databaseURL: "https://wunderfund-server.firebaseio.com",
+  projectId: "wunderfund-server",
+  storageBucket: "wunderfund-server.appspot.com",
+  messagingSenderId: "222886391134",
+  appId: "1:222886391134:web:82469ba2bcaf85037f33cf",
+  measurementId: "G-91DDD682S7",
+};
 
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const functions = firebase.functions();
-export const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
+export const timestamp = serverTimestamp();
+export const storage = getStorage(app);
 
-export async function uploadImage(refStr, file) {
+export async function uploadImage(reference, file) {
   try {
-    const ref = firebase.storage().ref().child(refStr);
-    const filer = await ref.put(file);
-    const url = await filer.ref.getDownloadURL();
-
-    return url;
+    const storageRef = ref(storage, reference);
+    const uploaded = await uploadBytes(storageRef, file);
+    return await getDownloadURL(uploaded.ref);
   } catch (error) {
-    return Error(error);
+    throw new Error(error);
   }
 }
-
-export const FacebookAuthProvider = new firebase.auth.FacebookAuthProvider();
-export const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
-// export const TwitterAuthProvider = new firebase.auth.TwitterAuthProvider();
 
 export async function downloadURL(reference, filename) {
   try {
-    const storageRef = firebase.storage().ref(reference);
-    const url = await storageRef.child(filename).getDownloadURL();
-    return url;
+    const storageRef = ref(storage, `${reference}/${filename}`);
+    return await getDownloadURL(storageRef);
   } catch (error) {
-    throw new Error(error.message);
+    throw new Error(error);
   }
 }
 
-export default firebase;
+export const googleAuthProvider = new GoogleAuthProvider();
+export const facebookAuthProvider = new FacebookAuthProvider();
